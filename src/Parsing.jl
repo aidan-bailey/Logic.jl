66601af3fd93a6b50b8970fbbc2c7ca7c7180047
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-10-01 20:30:34 +0200

    Add Parsing module

---
+++ src/Parsing.jl
@@ -0,0 +1,89 @@
+module Parsing
+
+using ..Types
+
+using ParserCombinator
+
+abstract type Node end
+struct Form <: Node val end
+struct Atom <: Node val end
+struct And <: Node val end
+struct Or <: Node val end
+struct Neg <: Node val end
+struct Impl <: Node val end
+struct Equiv <: Node val end
+struct Taut <: Node end
+struct Contr <: Node end
+
+process(root::Atom) = Types.Atom(root.val)
+
+process(root::Neg) = Types.not(process(root.val))
+
+process(::Taut) = Types.Tautology()
+
+process(::Contr) = Types.Contradiction()
+
+function process(root::Node)
+
+    processedChildren::Vector{Types.Formula} = map(process, root.val)
+
+    if length(processedChildren) == 1
+        return processedChildren[1]
+    end
+
+    precedences = [And, Or, Impl, Equiv]
+
+    for precop in precedences
+        i = 2
+        while i < length(root.val) + 1
+            op = root.val[i]
+            if op isa precop
+                deleteat!(root.val, i)
+                if op isa And
+                    processedChildren[i-1] = Types.and(processedChildren[i-1], processedChildren[i])
+                elseif op isa Or
+                    processedChildren[i-1] = Types.or(processedChildren[i-1], processedChildren[i])
+                elseif op isa Impl
+                    processedChildren[i-1] = Types.implies(processedChildren[i-1], processedChildren[i])
+                elseif op isa Equiv
+                    processedChildren[i-1] = Types.equals(processedChildren[i-1], processedChildren[i])
+                end
+                deleteat!(processedChildren, i)
+            else
+                i+=1
+            end
+        end
+    end
+
+    return processedChildren[1]
+
+end
+
+expr = Delayed()
+or = Delayed()
+and = Delayed()
+neg = Delayed()
+impl = Delayed()
+equiv = Delayed()
+
+atom = (E"T" > Taut) | (E"F" > Contr) | ((PInt() | PFloat64() | Word()) > Atom) | (E"(" + expr + E")" |> Form)
+
+neg.matcher = (E"!" + neg |> Neg) | atom
+
+and.matcher = (E"&" + and |> And) | neg
+
+or.matcher = (E"|" + or |> Or) | and
+
+impl.matcher = (E">" + impl |> Impl) | or
+
+equiv.matcher = (E"<>" + neg |> Equiv) | impl
+
+form = equiv
+
+expr.matcher = form[0:end]
+
+start = expr + Eos() |> Form
+
+parse(str::AbstractString)::Formula = process(parse_one(str, start)[1])
+
+end
--- src/PropositionalLogic.jl
+++ src/PropositionalLogic.jl
@@ -1,9 +1,11 @@
 module PropositionalLogic
 
 include("Types.jl")
+include("Parsing.jl")
 include("Algorithms.jl")
 
 using .Types
 using .Algorithms
+using .Parsing
 
 end # module
